7dfd6d2b606b1f35e6e56cf27efadd7db9732dfb
Dominik Jungowski <Dominik@Jungowski.de>
2013-06-11 20:58:01 +0200

    read csv

--- index-importer.php
+++ index-importer.php
@@ -12,3 +12,33 @@ $indexFilename = $options['f'];
 // Create DB Connection
 $dsn = sprintf('mysql:dbname=%s;host=%s', $mysql['dbname'], $mysql['host']);
 $db = new PDO($dsn, $mysql['user'], $mysql['password']);
+
+// Open CSV
+$filehandle = fopen($indexFilename, "r");
+$csvDelimiter = ',';
+$csvEnclosure = '"';
+$rowNumber = 0;
+$baseInsertSql = '
+INSERT INTO
+	country (
+		name,
+		currency,
+		ratio
+	)
+VALUES (
+	"%s",
+	"%s",
+	"%b"
+)
+';
+while (($row = fgetcsv($filehandle, 1000, $csvDelimiter, $csvEnclosure)) !== FALSE) {
+	$rowNumber++;
+	// Skip the first row as it contains the column names
+	if ($rowNumber == 1) {
+		continue;
+	}
+	$country = $row[0];
+	$currency = $row[1];
+	$ratio = substr($row[3], 0, -1);
+	var_dump($country, $currency, $ratio);
+}
