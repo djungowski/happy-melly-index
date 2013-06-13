0958eea028150266595d85887249578dc8898a22
Dominik Jungowski <Dominik@Jungowski.de>
2013-06-13 16:04:59 +0200

    removed unused code

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// MySQL
-$mysql = array(
-	'host'		=> 'localhost',
-	'dbname'	=> 'happy_melly_index',
-	'user'		=> 'root',
-	'password'	=> ''
-);
--- db.php
+++
@@ -1,7 +0,0 @@
-<?php
-function get_db_connection($mysql) {
-	// Create DB Connection
-	$dsn = sprintf('mysql:dbname=%s;host=%s', $mysql['dbname'], $mysql['host']);
-	$db = new PDO($dsn, $mysql['user'], $mysql['password']);
-	return $db;
-}
